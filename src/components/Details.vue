269cec063d1c7be4c17a0f40eacf881ee09d2f8e
GOD-SENT <oladayoahmod112@gmail.com>
2021-08-22 10:40:07 +0100

    added new components

---
+++ src/components/Details.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>hi there</h3>
+</template>
+
+<script>
+export default {
+	name : "Details"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <nav class="breadcrumb breadcrumb-style mt-5 text-center bg-light rounded neo-shadow">
+            <p class="text-center my-2 breadcrumb-item active">Developed by <span class="text-primary"> Unitech </span> &copy; copyright 2021</p>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "Footer"
+}
+</script>
+
+<style  scoped>
+.breadcrumb-style{
+    width : fill-available;
+    width: -moz-fill-available;
+    width :-webkit-fill-available;
+    position : static !important;
+    display : block !important;
+    text-align : center !important;
+    bottom :0 !important;
+    margin-bottom:0;
+}
+.neo-shadow{
+border: 0 !important;
+background-color: #e0e5ec !important;
+box-shadow: -7px -7px 20px 0px #fff9, -4px -4px 5px 0px #fff9, 7px 7px 20px 0px #0002, 4px 4px 5px 0px #0001, inset 0px 0px 0px 0px #fff9, inset 0px 0px 0px 0px #0001, inset 0px 0px 0px 0px #fff9, inset 0px 0px 0px 0px #0001;
+border-radius: 15px;
+/* padding: 20px 0px!important; */
+}
+</style>
